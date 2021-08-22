ee5934b9f4b0743211921e464c8066393a62eb17
Eric Badiere <ebadiere@gmail.com>
2021-08-22 17:50:54 -0600

    Initial commit.

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/dto/RegisterUserResponse.java
@@ -0,0 +1,9 @@
+package com.springbank.user.cmd.api.dto;
+
+public class RegisterUserResponse extends BaseResponse{
+    private String id;
+    public RegisterUserResponse(String id, String message) {
+        super(message);
+        this.id = id;
+    }
+}
